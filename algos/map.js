027246741de730059de4810c0c38ef5c53d4f728
Divyanshu Mathpal <divyanshumathpal@gmail.com>
2020-06-28 01:46:56 +0530

    map created

--- algos/map.js
+++ algos/map.js
@@ -1,8 +1,26 @@
-class map{
-    constructor(length,breadth,type){
-        this.length=length;
-        this.breadth=breadth;
-        this.type=type;
+class map {
+    constructor(length, breadth) {
+        this.length = length;
+        this.breadth = breadth;
+        this.matrix = [];
+        const default_array = []
+        for (let index = 0; index < length; index++) {
+            default_array.push('0');
+        }
+        for (let index = 0; index < breadth; index++) {
+            this.matrix.push(default_array);
+        }
+        // console.log(this.matrix);
+    }
+    print_map() {
+        for (let i = 0; i < this.breadth; i++) {
+            for (let j = 0; j < this.length; j++) {
+                process.stdout.write(this.matrix[i][j]);
+            }
+            process.stdout.write('\n');
+        }
     }
 };
-exports.map=map;
+var x = new map(50, 50);
+x.print_map();
+exports.map = map;
