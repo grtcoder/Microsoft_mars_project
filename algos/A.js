cfc1011f08a445073c949c824b7b81dc451a96d8
Divyanshu Mathpal <divyanshumathpal@gmail.com>
2020-06-28 00:55:14 +0530

    map attribuite class added

--- algos/A.js
+++ algos/A.js
@@ -1,6 +1,8 @@
 var point=require('./point.js')
 var adjacency_list=[]
-const point=
-// function findpath(adjlist,start,end) {
-    
-// }
+var pt=new point.point(1,2,3);
+console.log(pt);
+function findpath(adjlist,start,end) {
+    var open=[];
+    var closed=[start];
+};
---
+++ algos/map.js
@@ -0,0 +1,8 @@
+class map{
+    constructor(length,breadth,type){
+        this.length=length;
+        this.breadth=breadth;
+        this.type=type;
+    }
+};
+exports.map=map;
