ca2ee6961cb23276a3013c9a152cac195b851894
Mohit <2018eeb1165@iitrpr.ac.in>
2020-07-16 18:25:36 +0530

    A star added to views

--- server/api/views.py
+++ server/api/views.py
@@ -4,13 +4,10 @@ from rest_framework.response import Response
 from rest_framework import viewsets, permissions, authentication, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView, UpdateAPIView
+
 # Create your views here.
 @api_view(('POST',))
 def test(request):
-<<<<<<< HEAD
-    print(request.GET)
-    return Response({})
-=======
     print(request.POST)
+
     return Response({'a':'b'})
->>>>>>> b08d6d044c69939bcb0102000723ab0b385d8726
