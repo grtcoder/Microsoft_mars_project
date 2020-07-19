f81ee49e209a474517318c4b2d9f2bb93ffbeca6
Divyanshu Mathpal <divyanshumathpal@gmail.com>
2020-07-20 05:12:42 +0530

    done

--- server/settings.py
+++ server/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DATA_UPLOAD_MAX_MEMORY_SIZE = 1000000000
